f2fabf655b7e84049de0fb9e4b8d91721bdc79b6
Bien <bienx16@gmail.com>
2024-03-15 04:54:59 +0800

    updated

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Capture all anchor clicks
-  const anchors = document.querySelectorAll("a");
-  anchors.forEach((anchor) => {
-    anchor.addEventListener("click", function (event) {
-      event.preventDefault();
-      console.log("Clicked: ", event.target);
+function openModal(imageSrc) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-image");
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+}
+
+// JavaScript function to close the modal
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+// JavaScript function to capture all anchor clicks
+document
+  .querySelectorAll(".cards .card a, .read-more, .column.first a")
+  .forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent default action (e.g., navigating to a link)
+      if (this.hasAttribute("data-src")) {
+        var imageSrc = this.getAttribute("data-src");
+        openModal(imageSrc);
+      }
     });
   });
-
-  // Image modal functionality
-  const modal = document.getElementById("modal");
-  const modalImage = document.getElementById("modal-image");
-  const images = document.querySelectorAll(".image-container img");
-  const closeModal = document.querySelector(".close");
-
-  images.forEach((image) => {
-    image.addEventListener("click", function () {
-      modal.style.display = "block";
-      modalImage.src = this.src;
-    });
-  });
-
-  closeModal.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-
-  window.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-});
